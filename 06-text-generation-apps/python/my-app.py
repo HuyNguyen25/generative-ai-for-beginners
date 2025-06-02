f0c9cc6ee52b24dc265af38af658cabb8df67794
Nguyen Phuc Huy <124686333+HuyNguyen25@users.noreply.github.com>
2025-06-02 17:50:16 -0400

    Create my-app.py

---
+++ 06-text-generation-apps/python/my-app.py
@@ -0,0 +1,27 @@
+from openai import AzureOpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+client = AzureOpenAI(
+    api_key = os.environ['AZURE_OPENAI_API_KEY'],
+    azure_endpoint = os.environ['AZURE_OPENAI_ENDPOINT'],
+    api_version = os.environ['AZURE_OPENAI_API_VERSION']
+)
+
+deployment = os.environ['AZURE_OPENAI_DEPLOYMENT']
+
+prompt = "Who plays Jack Sparrow?"
+messages = [
+    {
+        "role": "user",
+        "content": prompt
+    }
+]
+
+completion = client.chat.completions.create(
+    model = deployment,
+    messages = messages
+)
+
+print(completion.choices[0].message.content)
